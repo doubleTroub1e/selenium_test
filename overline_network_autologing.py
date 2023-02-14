eca8fb67b34af0f7504e146c48a1546f03d26a93
MaxZubrytskyi <maksym.zubrytskyi@logivations.com>
2023-02-14 18:21:11 +0200

    make code more pretty

--- overline_network_autologing.py
+++ overline_network_autologing.py
@@ -2,20 +2,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from credentials import *
 import time
 import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--username',
-                    default='1',
-                    dest='user_c',
-                    help='Provide username to use. Defaults to overline_username_1',
-                    type=str
-                    )
-args = parser.parse_args()
 
 def get_valid_username(passed_value):
     if passed_value == "1":
@@ -40,82 +29,70 @@ def get_valid_password(passed_value):
         print("using default username and password")
         password = overline_password_1
     return (password)
-username = get_valid_username(args.user_c)
-password = get_valid_password(args.user_c)
-
-# place credentials here and uncomment it 
-#username = "paster login here"
-#password = "paster password here here"
 
 
-def check_exists_by_xpath(xpath):
-    try:
-        webdriver.find_element_by_xpath(xpath)
-    except NoSuchElementException:
-        return False
-    return True
+class OVERLINE():
+    def __init__(self, username, password, driver):
+        self.username = username
+        self.password = password
+        self.driver = driver
+        self.failed = []
 
-print("Test Execution Started, using username " + username )
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-ssl-errors=yes')
-options.add_argument('--ignore-certificate-errors')
-options.add_argument("--enable-javascript")
-driver = webdriver.Remote(
-command_executor='http://localhost:4444/wd/hub',
-options=options
-)
+    def open_web_site(self):
+        #opening overline.network
+        self.driver.get("https://overline.network/app/auth")
+        time.sleep(5)
+        self.login()
 
-def check_exists_by_CSS_SELECTOR( value):
-    try:
-        driver.find_element(By.CSS_SELECTOR, value)
-    except NoSuchElementException:
-        return False
-    return True
+    def login(self):
+        print("starting login process...")
+        self.driver.maximize_window()
 
+        #-------login process starts
+        #finding input boxes for username and password and pasing the appropriate values
+        self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='Email address']").send_keys(self.username)
+        self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='Password']").send_keys(self.password)
+        # get element
+        self.driver.find_element(By.XPATH, "//button[normalize-space()='Sign In']").click()
+        #-------login process ends
+        print("login success...")
+        self.close_pop_up()
 
-def open_web_site():
-    #opening overline.network
-    driver.get("https://overline.network/app/auth")
-    time.sleep(5)
-#    try:
-#        WebDriverWait(driver, 20).until(
-#            EC.presence_of_element_located((By.XPATH, "//button[normalize-space()='Sign In']"))
-#        )
-#    finally:
-#        driver.quit()
+    def close_pop_up(self):
+        time.sleep(7)
+        print("closing pop-up message now")
+        self.driver.find_element(By.CSS_SELECTOR, "img[alt='cross-btn']").click()
+        self.finish_test_close_everything()
 
+    def finish_test_close_everything(self):
+        print ("Closing window in 5s...")
+        time.sleep(5)
+        self.driver.close()
+        self.driver.quit()
 
-def login():
-    print("starting login process...")
-    driver.maximize_window()
+def main():
+    """A dummy docstring."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--username',
+        default='1',
+        dest='user_c',
+        help='Provide username to use. Defaults to overline_username_1',
+        type=str
+        )
+    args = parser.parse_args()
+    username = get_valid_username(args.user_c)
+    password = get_valid_password(args.user_c)
 
-    #-------login process starts
-    #finding input boxes for username and password and pasing the appropriate values
-    driver.find_element(By.CSS_SELECTOR, "input[placeholder='Email address']").send_keys(username)
-    driver.find_element(By.CSS_SELECTOR, "input[placeholder='Password']").send_keys(password)
-    # get element
-    driver.find_element(By.XPATH, "//button[normalize-space()='Sign In']").click()
-    #-------login process ends
-    print("login success...")
+    print("Test Execution Started, using username " + username )
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-ssl-errors=yes')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument("--enable-javascript")
+    driver = webdriver.Remote(
+    command_executor='http://localhost:4444/wd/hub',
+    options=options
+    )
+    OVERLINE(username, password, driver).open_web_site()
 
-def close_pop_up():
-    time.sleep(7)
-    driver.find_element(By.CSS_SELECTOR, "img[alt='cross-btn']").click()
-
-
-
-#def check_exists_by_xpath(xpath):
-#    try:
-#        webdriver.find_element_by_xpath(xpath)
-#    except NoSuchElementException:
-#        return False
-#    return True
-
-open_web_site()
-login()
-close_pop_up()
-
-time.sleep(3)
-driver.close()
-driver.quit()
-print("Test Execution Successfully Completed!")
+if __name__ == "__main__":
+    main()
--- ventdrop_autologin_and_claim.py
+++ ventdrop_autologin_and_claim.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from credentials import *
 import time
 
@@ -10,61 +11,69 @@ password = ventdrop_password
 # place credentials here and uncomment it 
 #username = "paster login here"
 #password = "paster password here here"
+class VENTDROP():
+    def __init__(self, username, password, driver):
+        self.username = username
+        self.password = password
+        self.driver = driver
+        self.failed = []
+
+    def open_web_site(self):
+        #opening overline.network
+        print("Opening window")
+        self.driver.get("https://ventdrop.com/login")
+        time.sleep(5)
+        self.login()
+
+    def login(self ):
+        print("starting login process...")
+        self.driver.maximize_window()
+
+        #-------login process starts
+        #finding input boxes for username and password and pasing the appropriate values
+        self.driver.find_element(By.ID, 'username').send_keys(self.username)
+        self.driver.find_element(By.ID, 'password').send_keys(self.password)
+        # get element
+        self.driver.find_element(By.NAME, 'submit').click()
+        #-------login process ends
+        print("login success...")
+        self.hit_reward_button()
+
+    def hit_reward_button(self):
+        time.sleep(7)
+        print("trying to hit REWARD button")
+        self.driver.find_element(By.LINK_TEXT, 'Rewards').click()
+        self.hit_claim_chips_button()
+
+    def hit_claim_chips_button(self):
+        time.sleep(7)
+        print("trying to hit 'Claim Chips' button")
+        try:
+            self.driver.find_element(By.ID, 'cntdwn').click()
+        except NoSuchElementException:
+            print("Failed to hit, button not found...")
+            return self.finish_test_close_everything()
+        return self.finish_test_close_everything()
+        
+    
+    def finish_test_close_everything(self):
+        print ("Closing window in 5s...")
+        time.sleep(5)
+        self.driver.close()
+        self.driver.quit()
 
 
+def main():
+    """A dummy docstring."""
+    print("Test Execution Started")
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-ssl-errors=yes')
+    options.add_argument('--ignore-certificate-errors')
+    driver = webdriver.Remote(
+    command_executor='http://localhost:4444/wd/hub',
+    options=options
+    )
+    VENTDROP(username, password, driver).open_web_site()
 
-print("Test Execution Started")
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-ssl-errors=yes')
-options.add_argument('--ignore-certificate-errors')
-driver = webdriver.Remote(
-command_executor='http://localhost:4444/wd/hub',
-options=options
-)
-print("Opening window")
-#maximize the window size
-driver.maximize_window()
-time.sleep(1)
-#navigate to browserstack.com
-driver.get("https://ventdrop.com/login")
-time.sleep(2)
-
-print("trying to inser username")
-uname = driver.find_element(By.ID, 'username')
-uname.send_keys(username)
-
-print("trying to inser pass")
-pword = driver.find_element(By.ID, 'password')
-pword.send_keys(password)
-
-time.sleep(1)
-
-print("trying to hit login button")
-driver.find_element(By.NAME, 'submit').click()
-print("waiting after login")
-time.sleep(2)
-
-
-print("trying to hit REWARD button")
-driver.find_element(By.LINK_TEXT, 'Rewards').click()
-time.sleep(7)
-
-print("trying to hit 'Claim Chips' button")
-
-## need to find out why this lixnk_text doens't work here, and how to make it work
-#driver.find_element(By.LINK_TEXT, 'Claim Chips').click()
-
-driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div[1]/div[2]/div[3]/p").click()
-
-#def check_exists_by_xpath(xpath):
-#    try:
-#        webdriver.find_element_by_xpath(xpath)
-#    except NoSuchElementException:
-#        return False
-#    return True
-
-time.sleep(5)
-driver.close()
-driver.quit()
-print("Test Execution Successfully Completed!")
-
+if __name__ == "__main__":
+    main()
